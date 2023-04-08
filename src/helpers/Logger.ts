f49733f01ea5b0951c297d1778ec6a1328bff5f3
muttaqin1 <mdmuttaqin20@gmail.com>
2023-04-08 17:21:29 +0600

    Add winston logger.

---
+++ src/helpers/Logger.ts
@@ -0,0 +1,12 @@
+import { fileOpitons, consoleOptions } from '@config/logger';
+import { createLogger, transports } from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+const fileTransport = new DailyRotateFile(fileOpitons);
+const consoleTransport = new transports.Console(consoleOptions);
+
+export default createLogger({
+  transports: [consoleTransport, fileTransport],
+  exceptionHandlers: [fileTransport],
+  exitOnError: false
+});
