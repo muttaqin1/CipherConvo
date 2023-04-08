6ca6df84dd4a6a2b23deb1639c9db3310bc35b51
muttaqin1 <mdmuttaqin20@gmail.com>
2023-04-08 16:00:17 +0600

    add success response class.

---
+++ src/helpers/ApiResponse/ApiSuccessResponse.ts
@@ -0,0 +1,32 @@
+import { BaseResponse, successResponseCodeTypes } from './BaseResponse';
+
+/**
+ * @class ApiSuccessResponse
+ * @description This class is used to send success response
+ */
+
+export default class ApiSuccessResponse extends BaseResponse {
+  public setCookie(name: string, expiry: number, payload: string): this {
+    this.res.cookie(name, payload, {
+      httpOnly: true,
+      signed: true,
+      secure: true,
+      maxAge: expiry
+    });
+    return this;
+  }
+
+  public removeCookie(name: string): this {
+    this.res.clearCookie(name);
+    return this;
+  }
+
+  public override send<T extends Record<string, any>>(data: T): void {
+    super.send(data);
+  }
+
+  public status(code: successResponseCodeTypes): this {
+    this.setResponseStatus = code;
+    return this;
+  }
+}
