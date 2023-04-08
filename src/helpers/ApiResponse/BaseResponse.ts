0156b0f6ca17891ceb0cfa0bfaf5c35a11c8e50a
muttaqin1 <mdmuttaqin20@gmail.com>
2023-04-08 15:24:20 +0600

    add Base response class.

---
+++ src/helpers/ApiResponse/BaseResponse.ts
@@ -0,0 +1,42 @@
+import { TErrorStatusCode } from '@helpers/AppError/errorStatusCodes';
+import { ExtractObjValueTypes } from '@helpers/types';
+import { Response } from 'express';
+
+export const SuccessResponseCodes = {
+  SUCCESS: 200,
+  NO_DATA: 201
+} as const;
+
+export type successResponseCodeTypes = ExtractObjValueTypes<
+  typeof SuccessResponseCodes
+>;
+
+type responseCodes = TErrorStatusCode | successResponseCodeTypes;
+
+/**
+ * @class BaseResponse
+ * @description This class is the base class for all the response classes
+ */
+export abstract class BaseResponse {
+  private ResponseStatus: responseCodes = 201;
+
+  private defaultMessage = 'No Data';
+
+  constructor(protected res: Response) {}
+
+  protected set setResponseStatus(code: responseCodes) {
+    this.ResponseStatus = code;
+  }
+
+  protected get getResponseStatus(): responseCodes {
+    return this.ResponseStatus;
+  }
+
+  protected send(data: object): void {
+    let responseData = data;
+    // if paramiter (data) is empty we will add a default Message
+    if (Object.keys(data).length <= 0)
+      responseData = { message: this.defaultMessage };
+    this.res.status(this.ResponseStatus).json(responseData);
+  }
+}
