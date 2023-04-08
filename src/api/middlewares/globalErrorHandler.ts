ebcfe1fb87df4bc89ecd577ab73da212b61201a6
muttaqin1 <mdmuttaqin20@gmail.com>
2023-04-08 16:38:56 +0600

    Add global error handler.

---
+++ src/api/middlewares/globalErrorHandler.ts
@@ -0,0 +1,25 @@
+import { environment, IsProduction } from '@config/index';
+import { IsApiError } from '@helpers/AppError/ApiError';
+import { errorStatusCodes } from '@helpers/AppError/errorStatusCodes';
+import { NextFunction, Request, Response } from 'express';
+import Logger from '@helpers/Logger';
+import ApiErrorResponse from '@helpers/ApiResponse/ApiErrorResponse';
+import ApiSuccessResponse from '@helpers/ApiResponse/ApiSuccessResponse';
+
+export default (
+  err: any,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  if (res.headersSent) return next(err);
+  if (IsApiError(err)) return new ApiErrorResponse(res).send(err);
+  if (!IsProduction()) {
+    Logger.error(err.stack);
+    return new ApiSuccessResponse(res)
+      .status(err.statusCode || errorStatusCodes.INTERNAL_SERVER_ERROR)
+      .send({ env: environment, message: err.message });
+  }
+  Logger.error(err.stack);
+  return new ApiErrorResponse(res).send(err);
+};
