8187a76844b75ebdf9b82cb0a0f0f70561afd783
muttaqin1 <mdmuttaqin20@gmail.com>
2023-04-28 16:29:36 +0600

    Add user repository.

---
+++ src/api/repositories/UserRepository.ts
@@ -0,0 +1,80 @@
+import IUserRepository, {
+  notChangeable,
+  UserIncludedRolesAndActivities
+} from '@interfaces/repository/IUserRepository';
+import User, { userInput, userOutput } from '@models/User';
+import { inject, injectable } from 'inversify';
+import TYPES from '@ioc/TYPES';
+import IUser from '@interfaces/models/IUser';
+import Role from '@models/Role';
+import Activity from '@models/Activity';
+
+@injectable()
+export default class UserRepository implements IUserRepository {
+  constructor(
+    @inject(TYPES.UserModel) private readonly userModel: typeof User,
+    @inject(TYPES.RoleModel) private readonly roleModel: typeof Role,
+    @inject(TYPES.ActivityModel) private readonly activityModel: typeof Activity
+  ) {}
+
+  public createUser(user: userInput): Promise<userOutput | null> {
+    return this.userModel.create(user);
+  }
+
+  public findUserById(
+    id: string
+  ): Promise<UserIncludedRolesAndActivities | null> {
+    return this.userModel.findByPk(id, {
+      include: [
+        { model: this.roleModel, as: 'roles' },
+        { model: this.activityModel, as: 'activities' }
+      ]
+    }) as Promise<UserIncludedRolesAndActivities | null>;
+  }
+
+  public findUserByEmail(
+    email: string
+  ): Promise<UserIncludedRolesAndActivities | null> {
+    return this.userModel.findOne({
+      where: { email },
+      include: [
+        { model: this.roleModel, as: 'roles' },
+        { model: this.activityModel, as: 'activities' }
+      ]
+    }) as Promise<UserIncludedRolesAndActivities | null>;
+  }
+
+  public findByUsername(
+    userName: string
+  ): Promise<UserIncludedRolesAndActivities | null> {
+    return this.userModel.findOne({
+      where: { userName },
+      include: [
+        { model: this.roleModel, as: 'roles' },
+        { model: this.activityModel, as: 'activities' }
+      ]
+    }) as Promise<UserIncludedRolesAndActivities | null>;
+  }
+
+  public updateUser<T extends Partial<Omit<IUser, notChangeable>>>(
+    userId: string,
+    data: T
+  ): Promise<[affectedCount: number]> {
+    return this.userModel.update(data, { where: { id: userId } });
+  }
+
+  public deleteUser(id: string): Promise<number> {
+    return this.userModel.destroy({ where: { id } });
+  }
+
+  public setRoleAndActivityId(
+    userId: string,
+    roleId: string,
+    activityId: string
+  ): Promise<[affectedCount: number]> {
+    return this.userModel.update(
+      { roleId, activityId },
+      { where: { id: userId } }
+    );
+  }
+}
---
+++ src/interfaces/repository/IUserRepository.ts
@@ -0,0 +1,37 @@
+import IUser from '@interfaces/models/IUser';
+import { activityOutput } from '@models/Activity';
+import { roleOutput } from '@models/Role';
+import User, { userInput, userOutput } from '@models/User';
+
+export type notChangeable =
+  | 'userName'
+  | 'email'
+  | 'id'
+  | 'createdAt'
+  | 'updatedAt'
+  | 'deletedAt';
+
+export interface UserIncludedRolesAndActivities extends userOutput {
+  roles: roleOutput;
+  activities: activityOutput;
+}
+export default interface IUserRepository {
+  createUser(user: userInput): Promise<userOutput | null>;
+  findUserById(id: string): Promise<UserIncludedRolesAndActivities | null>;
+  findUserByEmail(
+    email: string
+  ): Promise<UserIncludedRolesAndActivities | null>;
+  findByUsername(
+    username: string
+  ): Promise<UserIncludedRolesAndActivities | null>;
+  updateUser<T extends Partial<Omit<Required<IUser>, notChangeable>>>(
+    userId: string,
+    data: T
+  ): Promise<[affectedCount: number]>;
+  deleteUser(id: string): Promise<number>;
+  setRoleAndActivityId(
+    userId: string,
+    roleId: string,
+    activityId: string
+  ): Promise<[affectedCount: number]>;
+}
