9e4e0aa3700522cd02200afe85dd953f16fad76f
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-01 17:24:02 +0600

    Add jest config.

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  displayName: 'Chat-application-rest-api-tests',
+  verbose: true,
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests']
+};
