e1c12102d794be36e373ed9a6ae7d47b5ccccb87
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-02 18:32:35 +0600

    Add unit test case for verifyToken method in Jwt class.

--- tests/unit/helpers/Jwt/Jwt.test.ts
+++ tests/unit/helpers/Jwt/Jwt.test.ts
@@ -1,7 +1,12 @@
 import 'reflect-metadata';
 import Jwt from '../../../../src/helpers/Jwt';
 import IJwt from '../../../../src/interfaces/helpers/IJsonWebToken';
-import { InternalServerError } from '../../../../src/helpers/AppError/ApiError';
+import {
+  BadTokenError,
+  ForbiddenError,
+  InternalServerError,
+  TokenExpiredError
+} from '../../../../src/helpers/AppError/ApiError';
 import { readFileSpy } from './mock';
 import { JWT } from '../../../../src/config/index';
 
@@ -10,11 +15,13 @@ const jwtPayload = {
   email: 'test@gmail.com',
   userName: 'test1',
   roleId: '123',
+  iat:Math.floor(Date.now() / 1000),
   exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
   iss: JWT.iss,
   aud: JWT.aud,
   sub: JWT.sub
 };
+
 describe('Helper Class: Jwt', () => {
   describe('Method: generateToken', () => {
     let jwt: IJwt;
@@ -35,5 +42,55 @@ describe('Helper Class: Jwt', () => {
       expect(readFileSpy).toHaveBeenCalledTimes(1);
     });
   });
+  describe('Method: verifyToken', () => {
+    let jwt: IJwt;
+    beforeEach(() => {
+      readFileSpy.mockClear();
+      jwt = new Jwt();
+    });
+    it('should throw a InternalServerError.', async () => {
+      readFileSpy.mockRejectedValueOnce(Promise.resolve(false));
+      await expect(jwt.verifyToken('token')).rejects.toThrow(
+        InternalServerError
+      );
+      expect(readFileSpy).toHaveBeenCalledTimes(1);
+    });
+    it('should throw a BadTokenError.', async () => {
+      await expect(jwt.verifyToken('token')).rejects.toThrow(BadTokenError);
+      expect(readFileSpy).toHaveBeenCalledTimes(1);
+    });
+    it('should throw a TokenExpiredError', async () => {
+      const Payload = {
+        ...jwtPayload,
+        exp: Math.floor(Date.now() / 1000)
+      };
+      const token = await jwt.generateToken(Payload);
+      await expect(jwt.verifyToken(token)).rejects.toThrow(TokenExpiredError);
+      expect(readFileSpy).toHaveBeenCalledTimes(2);
+    });
+    it('should  throw a ForbiddenError', async () => {
+      const Payload = {
+        ...jwtPayload,
 
+        iat: Math.floor(Date.now() / 1000) + 10000,
+        nbf: Math.floor(Date.now() / 1000) + 20000
+      };
+      const token = await jwt.generateToken(Payload);
+      await expect(jwt.verifyToken(token)).rejects.toThrow(ForbiddenError);
+      expect(readFileSpy).toHaveBeenCalledTimes(2);
+    });
+    it('should throw a InternalServerError', async () => {
+      readFileSpy.mockRejectedValueOnce(
+        new Error('I am not an api specific error.')
+      );
+      await expect(jwt.verifyToken('token')).rejects.toThrow(
+        InternalServerError
+      );
+      expect(readFileSpy).toHaveBeenCalledTimes(1);
+    });
+    it('should verify the token', async ()=>{
+      const token = await jwt.generateToken(jwtPayload);
+      await expect(jwt.verifyToken(token)).resolves.toStrictEqual(jwtPayload)
+    })
+  });
 });
