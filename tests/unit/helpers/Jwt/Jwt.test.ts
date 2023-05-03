95a8d215179c3adcf6c444383f837e888d2fb60e
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-03 14:57:03 +0600

    Add unit test case for decode token method in jwt class.

--- tests/unit/helpers/Jwt/Jwt.test.ts
+++ tests/unit/helpers/Jwt/Jwt.test.ts
@@ -7,15 +7,16 @@ import {
   InternalServerError,
   TokenExpiredError
 } from '../../../../src/helpers/AppError/ApiError';
-import { readFileSpy } from './mock';
+import { readFileSpy, signSpy, verifySpy } from './mock';
 import { JWT } from '../../../../src/config/index';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 const jwtPayload = {
   userId: '123',
   email: 'test@gmail.com',
   userName: 'test1',
   roleId: '123',
-  iat:Math.floor(Date.now() / 1000),
+  iat: Math.floor(Date.now() / 1000),
   exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
   iss: JWT.iss,
   aud: JWT.aud,
@@ -27,6 +28,7 @@ describe('Helper Class: Jwt', () => {
     let jwt: IJwt;
     beforeEach(() => {
       readFileSpy.mockClear();
+      signSpy.mockClear();
       jwt = new Jwt();
     });
 
@@ -37,8 +39,26 @@ describe('Helper Class: Jwt', () => {
       );
       expect(readFileSpy).toHaveBeenCalledTimes(1);
     });
+    it('should throw a InternalServerError.', async () => {
+      readFileSpy.mockRejectedValueOnce(
+        new Error('I am not an api specific error.')
+      );
+      await expect(jwt.generateToken(jwtPayload)).rejects.toThrow(
+        InternalServerError
+      );
+      expect(readFileSpy).toHaveBeenCalledTimes(1);
+    });
     it('should generate a jwt token.', async () => {
       await expect(jwt.generateToken(jwtPayload)).resolves.toBeTruthy();
+      expect(readFileSpy).toHaveBeenCalledTimes(1);
+    });
+    it('should throw a InternalServerError.', async () => {
+      signSpy.mockImplementationOnce(() => {
+        throw new Error('I am not an api specific error.');
+      });
+      await expect(jwt.generateToken(jwtPayload)).rejects.toThrow(
+        InternalServerError
+      );
       expect(readFileSpy).toHaveBeenCalledTimes(1);
     });
   });
@@ -88,9 +108,44 @@ describe('Helper Class: Jwt', () => {
       );
       expect(readFileSpy).toHaveBeenCalledTimes(1);
     });
-    it('should verify the token', async ()=>{
+    it('should verify the token', async () => {
       const token = await jwt.generateToken(jwtPayload);
-      await expect(jwt.verifyToken(token)).resolves.toStrictEqual(jwtPayload)
-    })
+      await expect(jwt.verifyToken(token)).resolves.toStrictEqual(jwtPayload);
+    });
+  });
+  describe('Method: decodeToken', () => {
+    let jwt: IJwt;
+    beforeEach(() => {
+      jwt = new Jwt();
+      readFileSpy.mockClear();
+      verifySpy.mockClear();
+    });
+    it('should throw a InternalServerError.', async () => {
+      const token = await jwt.generateToken(jwtPayload);
+      readFileSpy.mockResolvedValueOnce(Promise.resolve(''));
+      await expect(jwt.decodeToken(token)).rejects.toThrow(InternalServerError);
+      expect(readFileSpy).toHaveBeenCalledTimes(2);
+    });
+    it('should throw a BadTokenError.', async () => {
+      verifySpy.mockImplementationOnce(() => {
+        throw new JsonWebTokenError('jwt malformed');
+      });
+      const token = await jwt.generateToken(jwtPayload);
+      await expect(jwt.decodeToken(token)).rejects.toThrow(BadTokenError);
+      expect(readFileSpy).toHaveBeenCalledTimes(2);
+    });
+    it('should throw a InternalServerError.', async () => {
+      verifySpy.mockImplementationOnce(() => {
+        throw new Error('I am not an api specific error.');
+      });
+      const token = await jwt.generateToken(jwtPayload);
+      await expect(jwt.decodeToken(token)).rejects.toThrow(InternalServerError);
+      expect(readFileSpy).toHaveBeenCalledTimes(2);
+    });
+    it('should decode the token', async () => {
+      const token = await jwt.generateToken(jwtPayload);
+      await expect(jwt.decodeToken(token)).resolves.toStrictEqual(jwtPayload);
+      expect(readFileSpy).toHaveBeenCalledTimes(2);
+    });
   });
 });
--- tests/unit/helpers/Jwt/mock.ts
+++ tests/unit/helpers/Jwt/mock.ts
@@ -1,2 +1,5 @@
 import fs from 'fs/promises';
+import jwt from 'jsonwebtoken';
 export const readFileSpy = jest.spyOn(fs, 'readFile');
+export const verifySpy = jest.spyOn(jwt, 'verify');
+export const signSpy = jest.spyOn(jwt, 'sign');
