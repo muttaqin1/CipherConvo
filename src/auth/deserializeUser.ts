0bd69faae2dcb8d72710c524cc292bd2eb157645
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-07 14:43:45 +0600

    Feat: add deserializeUser middleware for JWT validation.
    This middleware validates the JSON Web Token (JWT) sent in the HTTP request headers, ensuring that the user making the request is authenticated.
    By adding this middleware, we improve the security of our API by preventing unauthorized access to protected endpoints. This feature will be beneficial for users who need to access restricted data or functionality.

---
+++ src/auth/deserializeUser.ts
@@ -0,0 +1,41 @@
+import { AuthFailureError } from '@helpers/AppError/ApiError';
+import IAuthUtils from '@interfaces/helpers/IAuthUtils';
+import AuthTokenKeysRepository from '@repositories/AuthTokenKeysRepository';
+import TYPES from '@ioc/TYPES';
+import { Response, NextFunction } from 'express';
+import container from '@ioc/container';
+import IUserRepository from '@interfaces/repository/IUserRepository';
+import Request from '@interfaces/request';
+
+export const deserializeUser = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const authUtils = container.get<IAuthUtils>(TYPES.AuthUtils);
+    const authTokenKeysRepository = container.get<AuthTokenKeysRepository>(
+      TYPES.AuthTokenKeysRepository
+    );
+    const userRepository = container.get<IUserRepository>(TYPES.UserRepository);
+    // Verify access token
+    const payload = await authUtils.verifyAccessToken(req);
+    // Find user
+    const user = await userRepository.findUserById(payload.userId, {
+      role: true,
+      activity: true
+    });
+    if (!user) throw new AuthFailureError('User not found');
+    if (!payload.accessTokenKey) throw new AuthFailureError();
+    // Find access token key
+    const keys = await authTokenKeysRepository.findKeys({
+      userId: user.id,
+      accessTokenKey: payload.accessTokenKey
+    });
+    if (!keys) throw new AuthFailureError();
+    // set user to req
+    req.user = user;
+  } catch (e) {
+    next(e);
+  }
+};
