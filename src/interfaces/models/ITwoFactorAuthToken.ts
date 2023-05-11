292dbf0c8fd859ba784aca2574365da1731d19a9
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-11 14:09:23 +0600

    Add enum TokenType in ITwoFactorAuthToken.

--- src/interfaces/models/ITwoFactorAuthToken.ts
+++ src/interfaces/models/ITwoFactorAuthToken.ts
@@ -1,8 +1,18 @@
+import { ExtractObjValueTypes } from '@helpers/types';
+
+export const TokenType = {
+  VERIFY_ACCOUNT: 'VERIFY_ACCOUNT',
+  VERIFY_EMAIL: 'VERIFY_EMAIL',
+  RESET_PASSWORD: 'RESET_PASSWORD'
+} as const;
+export type TTokenType = ExtractObjValueTypes<typeof TokenType>;
+
 export default interface ITwoFactorAuthToken {
   id: string;
   userId: string;
   token: string;
   tokenType: string;
+  verified: boolean;
   tokenExpiry: Date;
   createdAt?: Date;
   updatedAt?: Date;
