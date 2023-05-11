fa61c00ab0d95b8a7009e900c7efa3236fcf56ac
muttaqin1 <mdmuttaqin20@gmail.com>
2023-05-11 16:17:52 +0600

    Add functionality to send account verification email in EmailService.

--- src/api/services/EmailService.ts
+++ src/api/services/EmailService.ts
@@ -1,14 +1,29 @@
 import nodemailer, { Transporter } from 'nodemailer';
-import { InternalServerError } from '@helpers/AppError/ApiError';
-import { smtp } from '@config/index';
-import { injectable } from 'inversify';
+import {
+  AuthFailureError,
+  BadRequestError,
+  InternalServerError,
+  IsApiError
+} from '@helpers/AppError/ApiError';
+import { smtp, clientURL, twoFactorAuthTokenExpiry } from '@config/index';
+import { injectable, inject } from 'inversify';
 import IEmailService from '@interfaces/service/IEmailService';
+import { randomBytes } from 'crypto';
+import ITwoFactorAuthTokenRepository from '@interfaces/repository/ITwoFactorAuthTokenRepository';
+import TYPES from '@ioc/TYPES';
+import IUserRepository from '@interfaces/repository/IUserRepository';
+import { TokenType, TTokenType } from '@interfaces/models/ITwoFactorAuthToken';
 
 @injectable()
 export default class EmailService implements IEmailService {
   private transporter: Transporter;
 
-  constructor() {
+  constructor(
+    @inject(TYPES.TwoFactorAuthTokenRepository)
+    private readonly twoFactorAuthTokenRepository: ITwoFactorAuthTokenRepository,
+    @inject(TYPES.UserRepository)
+    private readonly userRepository: IUserRepository
+  ) {
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -30,5 +45,43 @@ export default class EmailService implements IEmailService {
     this.transporter.sendMail(mailOptions, (error) => {
       if (error) throw new InternalServerError();
     });
+  }
+
+  private async generateVerificationToken(
+    userId: string,
+    type: TTokenType
+  ): Promise<string> {
+    const token = randomBytes(32).toString('hex');
+    await this.twoFactorAuthTokenRepository.deleteToken(userId);
+    await this.twoFactorAuthTokenRepository.createToken({
+      token,
+      tokenExpiry: twoFactorAuthTokenExpiry,
+      verified: false,
+      userId,
+      tokenType: type
+    });
+    return token;
+  }
+
+  public async sendAccountVerificationEmail(email: string): Promise<void> {
+    try {
+      const user = await this.userRepository.findUserByEmail(email, {
+        activity: true
+      });
+      if (!user || !user.activities)
+        throw new AuthFailureError('User not found');
+      if (!user.activities.accessRestricted)
+        throw new BadRequestError('Account is already verified');
+      const token = await this.generateVerificationToken(
+        user.id,
+        TokenType.VERIFY_ACCOUNT
+      );
+      const subject = 'Verify your account';
+      const text = `Please verify your account by clicking the link: ${clientURL}/api/v1/auth/verify-verification-token/${token}`;
+      this.sendEmail(email, subject, text);
+    } catch (err) {
+      if (IsApiError(err)) throw err;
+      else throw new InternalServerError();
+    }
   }
 }
--- src/interfaces/service/IEmailService.ts
+++ src/interfaces/service/IEmailService.ts
@@ -1,2 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export default interface IEmailService {}
+export default interface IEmailService {
+  sendAccountVerificationEmail(email: string): Promise<void>;
+}
